0a5c9f193c89553feffad06c21bef7bbdca54208
Eliza Plowden <elizaplowden@gmail.com>
2021-04-22 13:30:19 +0100

    added youtube api key

---
+++ src/apis/youtube.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyBqz1bpwfzS28hO4XdbzBmRJmmqLE_ZLEg'
+
+export default axios.create({
+  // later we can get our instance of axios and GET from it using /search
+  baseURL: 'https:///www.googleapis.com/youtube/v3',
+  // all the query string params we want added onto request
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY,
+  }
+});
