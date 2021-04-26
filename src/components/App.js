0f868c85372fd1603682d70f92ee27babf61bb11
Eliza Plowden <elizaplowden@gmail.com>
2021-04-26 12:36:41 +0100

    grid layout

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,11 @@ class App extends React.Component {
     });
 
 
-  this.setState({ videos: response.data.items });
+  this.setState({
+    videos: response.data.items,
+    // first item will be default video
+    selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = video => {
@@ -27,11 +31,19 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-          />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+             <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+                <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+                />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,7 +4,11 @@ import VideoItem from './VideoItem';
 const VideoList = ({ videos, onVideoSelect }) => {
   // this inner function will be called for each video in the array
   const renderedList = videos.map((video) => {
-    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+    return <VideoItem
+      key={video.id.videoId}
+      onVideoSelect={onVideoSelect}
+      video={video}
+      />;
   });
   // props.videos - array of different records
   return <div className="ui relaxed divided list">{renderedList}</div>;
