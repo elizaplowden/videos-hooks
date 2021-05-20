09c2625022f3399723631d533083355dc7dec210
Eliza Plowden <elizaplowden@gmail.com>
2021-05-20 18:42:37 +0100

    refactorerd

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,12 +5,12 @@ const SearchBar = ({ onFormSubmit }) => {
   // declare term that's used on line 9
   const [term, setTerm] = useState('');
 
-  // event handlers need to be defined as variables if outside of class / inside function instead
-  const onInputChange = event => {
-    // to update state in a funtion component, you can't use this and setState
-    // we have to use setter we got when we initialised a piece of state with useState hook
-    setTerm(event.target.value);
-  };
+  // // event handlers need to be defined as variables if outside of class / inside function instead
+  // const onInputChange = event => {
+  //   // to update state in a funtion component, you can't use this and setState
+  //   // we have to use setter we got when we initialised a piece of state with useState hook
+  //   setTerm(event.target.value);
+  // };
 
   const onSubmit = event => {
     // prevents it automatically submittting
@@ -26,23 +26,13 @@ const SearchBar = ({ onFormSubmit }) => {
           <input
             type="text"
             value={term}
-            onChange={onInputChange}
+            // event handler instead of separate function
+            onChange={(event) => setTerm(event.target.value)}
           />
         </div>
       </form>
     </div>
   );
 };
-
-class SearchBar extends React.Component {
-  // old way of setting state
-  // state = { term: ''};
-
-
-
-  render() {
-
-  }
-}
 
 export default SearchBar;
